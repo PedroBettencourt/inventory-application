092de644b9b47cace3e3f54e8866fd5124109ec0
PedroBettencourt <pedropbettencourt@gmail.com>
2025-05-15 15:13:10 +0000

    Add css styling

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = express();
 const albumRouter = require('./routes/albumRouter');
 const artistRouter = require('./routes/artistRouter');
 const genreRouter = require('./routes/genreRouter');
+const path = require('node:path')
 
-
+// EJS
 app.set('view engine', 'ejs');
+
+// CSS
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+// Forms
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.get("/", (req, res) => {
     res.render("homepage");
 });
@@ -15,7 +24,6 @@ app.use("/artist", artistRouter);
 app.use("/genre", genreRouter);
 app.get("/error", (req, res) => res.render("error"));
 app.get("/:a", (req, res) => res.redirect("/error"));
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}`));
